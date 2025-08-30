4e90aa45752330eefad2094ee3275af053605099
dineshsayana <sayanadinesh1998@gmail.com>
2025-08-30 17:06:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+This module contains a simple hello_world function for demonstration.
+"""
+
 def hello_world(msg):
     """Prints a hello message with the given text."""
     print("Hello",msg)
