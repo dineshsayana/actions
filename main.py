64df206ad9bd858b815537328c1ffc603375f67c
Dinesh Chandra Sayana <sayanadinesh1998@gmail.com>
2025-08-31 21:36:11 -0500

    added week1 prime function

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 This module contains a simple hello_world function for demonstration.
 """
 
+
 def hello_world(msg):
     """Prints a hello message with the given text."""
-    print("Hello",msg)
+    print("Hello", msg)
 
-hello_world('dinesh')
+
+hello_world("dinesh")
--- week1/day1_prime.py
+++ week1/day1_prime.py
@@ -1,1 +1,25 @@
+"""
+The functions in this module checks if a number is prime or not.
+"""
 
+import math
+
+
+def is_prime(num):
+    """
+    Check if a number is prime.
+
+    Args:
+        num (int): The number to check.
+    Returns:
+        bool: True if the number is prime else False.
+    """
+    if num <= 1:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+print(is_prime(11))
