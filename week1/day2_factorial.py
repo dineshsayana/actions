db4dd9b292f353464c1e1177ff5c97dd324fb38c
Dinesh Chandra Sayana <sayanadinesh1998@gmail.com>
2025-08-31 22:40:18 -0500

    Adding the factorial

--- week1/day2_factorial.py
+++ week1/day2_factorial.py
@@ -1,1 +1,37 @@
+"""
+This functions find the factorial of a number, In Two different apporaches "Iter" , "Recurisive".
+"""
 
+
+def factorial_iter(num: int) -> int:
+    """
+    Find the factorial of the given number
+    Args:
+        num (int): The number to find the factorial of.
+
+    Returns:
+        int: The factorial of the number.
+    """
+    result = 1
+    for i in range(1, num + 1):
+        result *= i
+
+    return result
+
+
+def factorial_recur(num: int) -> int:
+    """
+    Find the factorial of the given number using recursion.
+    Args:
+        num (int): The number to find the factorial of.
+
+    Returns:
+        int: The factorial of the number.
+    """
+    if num in (0, 1):
+        return 1
+    return num * factorial_recur(num - 1)
+
+
+print(factorial_iter(5))
+print(factorial_recur(5))
