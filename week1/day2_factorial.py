cee8c78b7ce596773598df5e3d2694e853aa72ab
Dinesh Chandra Sayana <sayanadinesh1998@gmail.com>
2025-08-31 23:18:55 -0500

    Added a class for a factorial

--- week1/day2_factorial.py
+++ week1/day2_factorial.py
@@ -1,37 +1,46 @@
 """
-This functions find the factorial of a number, In Two different apporaches "Iter" , "Recurisive".
+Class called factorial to find the factorial of a number, 
+In Two different apporaches "Iter" , "Recurisive".
 """
 
 
-def factorial_iter(num: int) -> int:
+class Factorial:
     """
-    Find the factorial of the given number
-    Args:
-        num (int): The number to find the factorial of.
-
-    Returns:
-        int: The factorial of the number.
+    Class to find the factorial of a number using iterative and recursive methods.
     """
-    result = 1
-    for i in range(1, num + 1):
-        result *= i
 
-    return result
+    def __init__(self):
+        pass
+
+    def factorial_iter(self, num: int) -> int:
+        """
+        Find the factorial of the given number
+        Args:
+            num (int): The number to find the factorial of.
+
+        Returns:
+            int: The factorial of the number.
+        """
+        result = 1
+        for i in range(1, num + 1):
+            result *= i
+
+        return result
+
+    def factorial_recur(self, num: int) -> int:
+        """
+        Find the factorial of the given number using recursion.
+        Args:
+            num (int): The number to find the factorial of.
+
+        Returns:
+            int: The factorial of the number.
+        """
+        if num in (0, 1):
+            return 1
+        return num * self.factorial_recur(num - 1)
 
 
-def factorial_recur(num: int) -> int:
-    """
-    Find the factorial of the given number using recursion.
-    Args:
-        num (int): The number to find the factorial of.
-
-    Returns:
-        int: The factorial of the number.
-    """
-    if num in (0, 1):
-        return 1
-    return num * factorial_recur(num - 1)
-
-
-print(factorial_iter(5))
-print(factorial_recur(5))
+factorial = Factorial()
+print(factorial.factorial_iter(5))
+print(factorial.factorial_recur(5))
